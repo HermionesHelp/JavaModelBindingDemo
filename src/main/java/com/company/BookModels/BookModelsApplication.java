289dcd64d263546d541b001659bc7d1aae282268
Kristan Frost <kristan.frost@gmail.com>
2022-05-17 12:30:48 -0500

    begin Models demo project

---
+++ src/main/java/com/company/BookModels/BookModelsApplication.java
@@ -0,0 +1,13 @@
+package com.company.BookModels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookModelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookModelsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/BookModels/BookModelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.BookModels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookModelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
